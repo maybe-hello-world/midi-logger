230f86a9c976d882ce938900aae8e6a590875f6e
maybe-hello-world <maybe.hello.world@gmail.com>
2024-05-30 03:15:19 -0700

    Initial implementation

--- main.ts
+++ main.ts
@@ -1,85 +1,113 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MarkdownView, Notice, Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+interface MIDILoggerSettings {}
+const DEFAULT_SETTINGS: MIDILoggerSettings = {}
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+export default class MIDILogger extends Plugin {
+	settings: MIDILoggerSettings;
+	enabled: boolean = false;
+	mainStatusBar: HTMLElement;
+	midiAccess: WebMidi.MIDIAccess;
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	writeNoteToEditor(note: number) {
+		const noteNames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+		const noteName = noteNames[note % 12];
+		const octave = Math.floor(note / 12) - 1;
+		const noteString = `${noteName}${octave},`;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (view) {
+			const cursor = view.editor.getCursor();
+			view.editor.replaceRange(noteString, cursor);
+			view.editor.setCursor(cursor.line, cursor.ch + noteString.length);
+		}
+	}
+
+	async enable() {
+		try {
+			this.midiAccess = await navigator.requestMIDIAccess();
+			const inputs = this.midiAccess.inputs.values();
+			for (let input of inputs) {
+				input.onmidimessage = (msg) => {
+					const [status, data1, data2] = msg.data;
+					const command = status >> 4;
+					const channel = status & 0xf;
+	
+					if (command === 0x9 && channel === 0) {
+						const note = data1;
+						const velocity = data2;
+						if (velocity > 0) {
+							this.writeNoteToEditor(note);
+						}
+					}
+				};
+			}
+			new Notice('MIDI Logger is active');
+			this.mainStatusBar.setText('MIDI Logger is active');
+			this.enabled = true;
+		} catch (error) {
+			new Notice('Cannot open MIDI input port!');
+		}
+	}
+
+	disable() {
+		if (this.midiAccess) {
+			const inputs = this.midiAccess.inputs.values();
+			for (let input of inputs) {
+				input.close();
+			}
+		}
+		new Notice('MIDI Logger is inactive');
+		this.mainStatusBar.setText('');
+		this.enabled = false;
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.mainStatusBar = this.addStatusBarItem();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.addRibbonIcon('music', 'MIDI Logger', (evt: MouseEvent) => {
+			if (this.enabled) {
+				this.disable();
+			} else {
+				this.enable();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: "enable",
+			name: "Enable MIDI Logger",
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
+				if (!this.enabled) {
 					if (!checking) {
-						new SampleModal(this.app).open();
+						this.enable();
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					return true
 				}
-			}
-		});
+				return false;
+			},
+		  });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addCommand({
+			id: "disable",
+			name: "Disable MIDI Logger",
+			checkCallback: (checking: boolean) => {
+				if (this.enabled) {
+					if (!checking) {
+						this.disable();
+					}
+					return true
+				}
+				return false;
+			},
+		  });
 	}
 
 	onunload() {
-
+		if (this.enabled) {
+			this.disable();
+		}
 	}
 
 	async loadSettings() {
@@ -88,47 +116,5 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
